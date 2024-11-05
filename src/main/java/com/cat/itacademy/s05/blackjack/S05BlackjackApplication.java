2fc80596db47a9fbad60750009191ddd102a18f5
jraporta <jraporta@gmail.com>
2024-11-05 13:05:51 +0100

    initial commit

---
+++ src/main/java/com/cat/itacademy/s05/blackjack/S05BlackjackApplication.java
@@ -0,0 +1,13 @@
+package com.cat.itacademy.s05.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05BlackjackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05BlackjackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cat/itacademy/s05/blackjack/S05BlackjackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cat.itacademy.s05.blackjack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05BlackjackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
